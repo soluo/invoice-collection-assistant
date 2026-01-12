1c036bb4250926178659913541d5ce6fda9b4141
Julien Casanova <juliencasanova@gmail.com>
2026-01-12 14:39:02 +0100

    fix: wrong react-router import

--- src/components/modals/EmailPreviewModal.tsx
+++ src/components/modals/EmailPreviewModal.tsx
@@ -8,7 +8,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 interface EmailPreviewModalProps {
   open: boolean;
